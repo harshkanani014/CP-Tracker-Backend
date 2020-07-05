e0a0e54954ec8c6066fc8da5fa8cb82b09faa187
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-05 20:15:52 +0530

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CP-Track',
         'USER': 'postgres',
-        'PASSWORD': 'janu5ulka',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
