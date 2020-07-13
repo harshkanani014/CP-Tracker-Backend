b6074f3034d059c5318f57f3faa82a1ed0e42bad
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-13 23:18:56 +0530

    fix

--- backend/problempage/urls.py
+++ backend/problempage/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import CPproblemListCreate, CPtutorialListCreate, pythonproblemListCreate, pythontutorialListCreate,    CPPproblemListCreate, CPPtutorialListCreate
-
+from .views import CPproblemListCreate, CPtutorialListCreate, pythonproblemListCreate, pythontutorialListCreate, CPPproblemListCreate, CPPtutorialListCreate
 
 
 urlpatterns = [
     path('api/postcard/cp/cp-problem', CPproblemListCreate.as_view()),
     path('api/postcard/cp/cp-tutorial', CPtutorialListCreate.as_view()),
-    path('api/postcard/python/python-problem', pythonproblemListCreate.as_view()),
-    path('api/postcard/python/python-tutorial', pythontutorialListCreate.as_view()),
+    path('api/postcard/python/python-problem',
+         pythonproblemListCreate.as_view()),
+    path('api/postcard/python/python-tutorial',
+         pythontutorialListCreate.as_view()),
     path('api/postcard/CPP/CPP-problem', CPPproblemListCreate.as_view()),
     path('api/postcard/CPP/CPP-tutorial', CPPtutorialListCreate.as_view())
 ]
